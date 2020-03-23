215caf9a80002d8ba04ce746af01a0177520e9af
Dario <dkemenovic@gmail.com>
2020-03-23 21:12:33 +0100

    Create Homepage component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./img/logo.jpg";
 import "./App.css";
 
@@ -6,21 +6,30 @@ const addActivity = () => {
   alert("test");
 };
 
+const Homepage = () => {
+  return (
+    <header className="Homepage">
+      <img src={logo} className="Homepage" alt="logo" />
+      <div className="container">
+        <div className="left">
+          <img src="screenshot" className="screenshot" alt="screenshot" />
+        </div>
+        <div className="right">
+          <p>Log your daily activities</p>
+          <button onClick={addActivity}>Start</button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
 function App() {
+  const [screen, setScreen] = useState("homepage");
+
   return (
     <div className="App">
-      <header className="Homepage">
-        <img src={logo} className="Homepage" alt="logo" />
-        <div className="container">
-          <div className="left">
-            <img src="screenshot" className="screenshot" alt="screenshot" />
-          </div>
-          <div className="right">
-            <p>Log your daily activities</p>
-            <button onClick={addActivity}>Start</button>
-          </div>
-        </div>
-      </header>
+      {screen === "homepage" && <Homepage />}
+      {screen === "addActivity" && <addActivity />}
     </div>
   );
 }
