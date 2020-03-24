0afdd60a02e13c8a90d449079f467b7ee0ef5327
Dario <dkemenovic@gmail.com>
2020-03-24 21:31:08 +0100

    Import Homepage component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,6 @@
 import React, { useState } from "react";
-import logo from "./img/logo.jpg";
 import "./App.css";
-
-const Homepage = ({ setScreen }) => {
-  const addActivity = () => {
-    setScreen("addActivity");
-  };
-
-  return (
-    <header className="Homepage">
-      <img src={logo} className="Homepage" alt="logo" />
-      <div className="container">
-        <div className="left">
-          <img src="screenshot" className="screenshot" alt="screenshot" />
-        </div>
-        <div className="right">
-          <p>Log your daily activities</p>
-          <button onClick={addActivity}>Start</button>
-        </div>
-      </div>
-    </header>
-  );
-};
+import Homepage from "./components/Homepage";
 
 const AddActivity = () => {
   return <p>Add Activity</p>;
