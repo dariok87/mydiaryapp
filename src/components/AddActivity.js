e84fcf6f8b0d8329d9c2c3e5d684e53c711dc37d
Dario <dkemenovic@gmail.com>
2020-03-27 16:00:34 +0100

    Add onChange event

--- src/components/AddActivity.js
+++ src/components/AddActivity.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddActivity = () => {
+  const [title, setTitle] = useState("");
+
   const saveActivity = () => {};
+
   return (
     <div className="AddActivity">
       <h1>Activity title</h1>
-      <input />
+      <input onChange={e => setTitle(e.target.value)} />
       <h1>Activity description</h1>
       <textarea></textarea>
       <button onClick={saveActivity}>Save Activity</button>
