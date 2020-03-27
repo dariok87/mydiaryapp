4362f7dfbcf05ed30aa3d3dadf5fba007349fb56
Dario <dkemenovic@gmail.com>
2020-03-27 15:03:26 +0100

    Add input and textarea

--- src/components/AddActivity.js
+++ src/components/AddActivity.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 const AddActivity = () => {
-  return <p>Add Activity</p>;
+  return (
+    <div className="AddActivity">
+      <h1>Activity title</h1>
+      <input />
+      <h1>Activity description</h1>
+      <textarea></textarea>
+    </div>
+  );
 };
 
 export default AddActivity;
