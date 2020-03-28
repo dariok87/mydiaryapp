e6e3e3cf776434e0eebee568037b82fb2a2a1790
Dario <dkemenovic@gmail.com>
2020-03-28 13:58:36 +0100

    Import IndexedDB

--- src/App.js
+++ src/App.js
@@ -2,6 +2,22 @@ import React, { useState } from "react";
 import "./App.css";
 import Homepage from "./components/Homepage";
 import AddActivity from "./components/AddActivity";
+import { openDB } from "idb";
+
+const storeActivity = async ({ title, description }) => {
+  console.log(title, description);
+
+  const dbName = "diary.lol";
+  const storeName = "activities";
+  const version = 1;
+
+  await openDB(dbName, version, {
+    upgrade(db, oldVersion, newVersion, transaction) {
+      const store = db.createObjectStore(storeName, { autoIncrement: true });
+      store.put(activity);
+    }
+  });
+};
 
 const App = () => {
   const [screen, setScreen] = useState("homepage");
