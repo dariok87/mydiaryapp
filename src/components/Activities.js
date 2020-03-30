2c3b0e3180243bc271f856a8f057ba13ac42b74b
Dario <dkemenovic@gmail.com>
2020-03-30 18:57:03 +0200

    Map activities array

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -3,11 +3,15 @@ import React from "react";
 const Activities = ({ activities }) => {
   return (
     <div className="Activities">
-      <div className="List"></div>
+      <ul className="List">
+        {activities.map((activity, index) => {
+          return <li key={index}>{activity.title}</li>;
+        })}
+      </ul>
 
       <div className="Detail"></div>
     </div>
   );
 };
 
-export default ActivitiesList;
+export default Activities;
