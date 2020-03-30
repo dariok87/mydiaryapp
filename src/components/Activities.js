75bfdfd80ec772f6cd7836b6ad374f130348d68e
Dario <dkemenovic@gmail.com>
2020-03-30 18:43:47 +0200

    Create Activities component

---
+++ src/components/Activities.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Activities = ({ acttivities }) => {
+  return (
+    <div className="Activities">
+      <div className="List"></div>
+
+      <div className="Detail"></div>
+    </div>
+  );
+};
+
+export default ActivitiesList;
