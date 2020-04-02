aff4150e2adcbb6248c80433cb77cb8db65a3106
Dario <dkemenovic@gmail.com>
2020-04-02 20:54:34 +0200

    Import CSS-in-JS emotion

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -1,8 +1,16 @@
-import React from "react";
+//import React from "react";
+
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
 
 const Activities = ({ activities }) => {
   return (
-    <div className="Activities">
+    <div
+      className="Activities"
+      css={css`
+        font-size: 26px;
+      `}
+    >
       <ul className="List">
         {activities.map((activity, index) => {
           return <li key={index}>{activity.title}</li>;
